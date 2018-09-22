5bd5a06e529007905a186249998c76d937dc7f1a
Dan Chung <danetix@gmail.com>
2018-09-22 15:30:53 -0400

    changed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
   post "/api", to: "graphql#execute"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # namespace :api do
-  #   namespace :v1 do
-  #     resources :restaurants
-  #   end
-  # end
-  # resources :restaurants
 end
