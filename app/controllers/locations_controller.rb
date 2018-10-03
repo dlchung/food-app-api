f78ba66dd25401375438d2d1c387248c5a0a6ebe
Dan Chung <danetix@gmail.com>
2018-10-03 19:03:48 -0400

    add/delete location endpoints

---
+++ app/controllers/locations_controller.rb
@@ -0,0 +1,16 @@
+class LocationsController < ApplicationController
+  def create
+    name = params[:name]
+    address = params[:address]
+    location = Location.create(name:name, address: address)
+
+    render json: location
+  end
+
+  def delete
+    id = params[:id]
+    location = Location.find(id).delete
+
+    render json: location
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   post "/api/nearby", to:"restaurants#nearby"
   post "/api/rating", to:"restaurants#rating"
+  post "/api/locations/create", to:"locations#create"
+  post "/api/locations/delete", to:"locations#delete"
   post "/api", to: "graphql#execute"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
