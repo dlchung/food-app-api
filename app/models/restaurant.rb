dd7aeb5e9c94861d47f269dc2919a6f1d34d229f
Dan Chung <danetix@gmail.com>
2018-10-16 12:22:09 -0400

    refactor

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -9,7 +9,7 @@ class Restaurant < ApplicationRecord
   belongs_to :third_party_rating, required: false
 
   def set_third_party_rating(platform, force = false)
-    rating = ""
+    rating = ''
 
     if self.third_party_rating
       # puts "RATING OBJ EXISTS"
@@ -42,13 +42,13 @@ class Restaurant < ApplicationRecord
 
   def associate_platform(platform)
     case platform
-      when "yelp"
+      when 'yelp'
         associate_yelp
-      when "foursquare"
+      when 'foursquare'
         associate_foursquare
-      when "googleplaces"
+      when 'googleplaces'
         associate_googleplaces
-      when "zomato"
+      when 'zomato'
         associate_zomato
       else
     end
@@ -59,19 +59,19 @@ class Restaurant < ApplicationRecord
     lat = self.lat
     lng = self.lng
     limit = 1
-    categories = "restaurants, All"
+    categories = 'restaurants, All'
     url = "https://api.yelp.com/v3/businesses/search?term=#{term}&latitude=#{lat}&longitude=#{lng}&limit=#{limit}&categories=#{categories}"
     headers = { :Authorization => "Bearer #{YELP_API_KEY}" }
 
     response = JSON.parse(RestClient.get url, headers)
 
-    if response["businesses"].length > 0
-      self.yelp_url = response["businesses"].first["url"]
-      self.yelp_id = response["businesses"].first["id"]
+    if response['businesses'].length > 0
+      self.yelp_url = response['businesses'].first['url']
+      self.yelp_id = response['businesses'].first['id']
       self.save
 
-      if response["businesses"].first["rating"]
-        response["businesses"].first["rating"]
+      if response['businesses'].first['rating']
+        response['businesses'].first['rating']
       else
         0
       end
@@ -81,26 +81,26 @@ class Restaurant < ApplicationRecord
   end
 
   def associate_foursquare
-    v = "20180928"
+    v = '20180928'
     ll = "#{self.lat},#{self.lng}"
-    intent = "match"
+    intent = 'match'
     limit = 1
     name = self.name.parameterize
 
     url = "https://api.foursquare.com/v2/venues/search?client_id=#{FOURSQUARE_CLIENT_ID}&client_secret=#{FOURSQUARE_CLIENT_SECRET}&v=#{v}&ll=#{ll}&intent=#{intent}&limit=#{limit}&name=#{name}"
     response = JSON.parse(RestClient.get url)
 
-    if response["response"]["venues"].length > 0
-      foursquare_id = response["response"]["venues"].first["id"]
+    if response['response']['venues'].length > 0
+      foursquare_id = response['response']['venues'].first['id']
       self.foursquare_id = foursquare_id
 
       details_url = "https://api.foursquare.com/v2/venues/#{foursquare_id}?client_id=#{FOURSQUARE_CLIENT_ID}&client_secret=#{FOURSQUARE_CLIENT_SECRET}&v=#{v}"
       details_response = JSON.parse(RestClient.get details_url)
-      self.foursquare_url = details_response["response"]["venue"]["canonicalUrl"]
+      self.foursquare_url = details_response['response']['venue']['canonicalUrl']
       self.save
 
-      if details_response["response"]["venue"]["rating"]
-        details_response["response"]["venue"]["rating"]
+      if details_response['response']['venue']['rating']
+        details_response['response']['venue']['rating']
       else
         0
       end
@@ -149,16 +149,16 @@ class Restaurant < ApplicationRecord
     lon = self.lng
     count = 1
     radius = 30
-    sort = "real_distance"
+    sort = 'real_distance'
     url = "https://developers.zomato.com/api/v2.1/search?q=#{q}&lat=#{lat}&lon=#{lon}&radius=#{radius}&count=#{count}&sort=#{sort}"
     headers = { "user-key": ZOMATO_USER_KEY }
     response = JSON.parse(RestClient.get url, headers)
-    if response["restaurants"].length > 0
-      self.zomato_url = response["restaurants"].first["restaurant"]["url"]
-      self.zomato_id = response["restaurants"].first["restaurant"]["id"]
+    if response['restaurants'].length > 0
+      self.zomato_url = response['restaurants'].first['restaurant']['url']
+      self.zomato_id = response['restaurants'].first['restaurant']['id']
       self.save
 
-      response["restaurants"].first["restaurant"]["user_rating"]["aggregate_rating"]
+      response['restaurants'].first['restaurant']['user_rating']['aggregate_rating']
     else
       0
     end
