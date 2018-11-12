ccff032b559e0ec3cdfaca29e2b7d9d928d58a5a
Dan Chung <danetix@gmail.com>
2018-11-12 16:51:23 -0500

    testing cors

--- config/application.rb
+++ config/application.rb
@@ -35,8 +35,10 @@ module FoodAppApi
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins Rails.application.config.allowed_cors_origins
-        
-        resource '*', headers: :any, methods: [:get, :post, :options]
+
+        resource '*', 
+          headers: :any, 
+          methods: [:get, :post, :options]
       end
     end
   end
