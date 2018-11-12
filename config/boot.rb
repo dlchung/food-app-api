ec40cfa84e15d514552f2aceb1b9910a67760ef5
Dan Chung <danetix@gmail.com>
2018-11-12 18:48:27 -0500

    db prod

--- config/boot.rb
+++ config/boot.rb
@@ -1,5 +1,4 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
-require 'active_support/dependencies'
 require 'bundler/setup' # Set up gems listed in the Gemfile.
 require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
