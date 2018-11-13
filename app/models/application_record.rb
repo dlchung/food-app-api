acfc184e33af1e6a076c3d1afb88d796ea3f3eec
Dan Chung <danetix@gmail.com>
2018-11-12 19:20:41 -0500

    still troubleshooting heroku boot issues

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,9 @@
 class ApplicationRecord < ActiveRecord::Base
+  GOOGLE_PLACES_API_KEY = Rails.application.credentials.google[:places][:api_key]
+  YELP_API_KEY = Rails.application.credentials.yelp[:api_key]
+  FOURSQUARE_CLIENT_ID = Rails.application.credentials.foursquare[:client_id]
+  FOURSQUARE_CLIENT_SECRET = Rails.application.credentials.foursquare[:client_secret]
+  ZOMATO_USER_KEY = Rails.application.credentials.zomato[:user_key]
+  
   self.abstract_class = true
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,9 +1,3 @@
-GOOGLE_PLACES_API_KEY = Rails.application.credentials.google[:places][:api_key]
-YELP_API_KEY = Rails.application.credentials.yelp[:api_key]
-FOURSQUARE_CLIENT_ID = Rails.application.credentials.foursquare[:client_id]
-FOURSQUARE_CLIENT_SECRET = Rails.application.credentials.foursquare[:client_secret]
-ZOMATO_USER_KEY = Rails.application.credentials.zomato[:user_key]
-
 class Restaurant < ApplicationRecord
   has_many :reviews
   belongs_to :third_party_rating, required: false
