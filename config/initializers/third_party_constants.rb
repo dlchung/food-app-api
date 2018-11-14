5994c8443ff1ce427c5efe8783693bda1d5eceb1
Dan Chung <danetix@gmail.com>
2018-11-14 12:53:49 -0500

    fixed cors, moved constants to initializer

---
+++ config/initializers/third_party_constants.rb
@@ -0,0 +1,5 @@
+GOOGLE_PLACES_API_KEY = Rails.application.credentials.google[:places][:api_key]
+YELP_API_KEY = Rails.application.credentials.yelp[:api_key]
+FOURSQUARE_CLIENT_ID = Rails.application.credentials.foursquare[:client_id]
+FOURSQUARE_CLIENT_SECRET = Rails.application.credentials.foursquare[:client_secret]
+ZOMATO_USER_KEY = Rails.application.credentials.zomato[:user_key]
